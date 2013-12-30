4d95c1cba91f41f24141fe4d2d8b057791bbd70c
Josef M <lotus_mud@gmx.de>
2013-12-30 02:56:07 +0100

    extract header and footer (templates)

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,4 @@
-var html = "<div class='footer' data-role='footer' data-id='footer' data-position='fixed'>" +
+var footer_html = "<div class='footer' data-role='footer' data-id='footer' data-position='fixed'>" +
     "    <div>" +
     "    <p>Ihr Gerät ist mit dem Account <span class='footer_email'></span> verbunden.</p>" +
     "    </div>" +
---
+++ js/header.js
@@ -0,0 +1,49 @@
+var html = "<div id='header' class='header' data-role='header' data-position='fixed' data-id='header'>" +
+    "    <div class='header-text'>FH-Brandenburg App - Student</div>" +
+    "    <div data-iconpos='top' data-role='navbar' class='nav-example'>" +
+    "        <ul>" +
+    "            <li><a class='ui-btn-active ui-state-persist' id='home' data-icon='custom' href='#' onclick='showHomePage()'" +
+    "            data-transition='none'>Home</a></li>" +
+    "            <li><a id='chat' data-icon='custom' href='#page_chat' data-transition='none'>Chat</a></li>" +
+    "            <li><a id='contact' data-icon='custom' href='#page_contacts' data-transition='none'>Kontakte</a>" +
+    "            </li>" +
+    "            <li><a id='group' data-icon='custom' href='#page_groups' data-transition='none'>Gruppen</a></li>" +
+    "        </ul>" +
+    "    </div>" +
+    "    </div>";    <!-- /header -->
+
+
+
+
+function showHeader(pageID, activePage, pageTitle) {
+    var myarray = {
+        "home":"Home",
+        "chat":"Chat",
+        "contacts":"Kontakte",
+        "groups":"Gruppen",
+        "logout":""
+    };
+
+    var header_html = '<div id="header" class="header" data-role="header" data-position="fixed" data-id="header">';
+    header_html+=' <div class="header-text">'+pageTitle+'</div>';
+    header_html+='<div data-iconpos="top" data-role="navbar" class="nav-example"><ul>';
+    var _insert = "";
+    for (var key in myarray) {
+        if (key == activePage)
+            _insert="ui-btn-active ui-state-persist";
+        else _insert = "";
+        if (key == "home")
+            header_html+='<li><a id="'+key+'" data-icon="custom" href="#" onclick="showHomePage()" class="'+_insert+'" data-transition="none">'+myarray[key]+'</a></li>';
+        else
+            header_html+='<li><a id="'+key+'" data-icon="custom" href="#page_'+key+'" class="'+_insert+'" data-transition="none">'+myarray[key]+'</a></li>';
+    }
+
+    header_html+='</ul></div></div><!-- /header -->';
+
+    $("div[id='"+pageID+"']").append(header_html);
+    console.log(header_html);
+    //$("#"+pageID).prepend(header_html);
+
+  //  if (activePage != "")
+   //     $("#"+activePage).addClass("ui-btn-active ui-state-persist");
+}
