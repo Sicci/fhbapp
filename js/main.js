25898554c8f8b38da3f98d8bdf08779cc7e1a8cc
Josef M <lotus_mud@gmx.de>
2014-01-17 16:20:49 +0100

    fix spelling 2

--- js/main.js
+++ js/main.js
@@ -1427,7 +1427,7 @@ function scanCode() {
 function updateAttendance(qrcontent) {
     $.ajax({url: url + "update/attendance",
         dataType: "jsonp",
-        data: {ssid:currentSSID, eqrcontent:qrcontent},
+        data: {ssid:currentSSID, qrcontent:qrcontent},
         async: true,
         success: function (result) {
             if (isSessionValid(result.updateattendance)) {
